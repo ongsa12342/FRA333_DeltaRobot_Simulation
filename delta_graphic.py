2fd81437ad9c9b6305128f4f29d4404ade31591e
ongsa12342 <61608951+ongsa12342@users.noreply.github.com>
2023-12-02 00:41:13 +0700

    camera setting

--- delta_graphic.py
+++ delta_graphic.py
@@ -7,6 +7,7 @@ link_length = 200
 frame_size = 346.41
 
 eff_link = 44.95
+eff_link_size = 2
 
 class BASE:
     def __init__(self, ball_size = ball_size, link_size=link_size, link_length=link_length, frame_size=frame_size):
@@ -139,26 +140,26 @@ class RE:
         # self.re3.axis = vector(pos[0] , pos[1], pos[2]) -  self.re3.pos+ vector(1,1,0)
 
 class EFF:
-    def __init__(self, frame_size=frame_size):
-        self.sphere_object = sphere(pos=vector(0, 0, 0), radius=ball_size, color=color.blue)
-
+    def __init__(self, eff_link_size=eff_link_size):
+        self.sphere_object = sphere(pos=vector(0, 0, 0), radius=ball_size, color=color.red)
+        self.eff_link_size = eff_link_size
         self.frame1 = cylinder(
             pos=vector(0, 0, 0),
             axis=vector(0, 0, 0),
-            radius=link_size,
-            color=color.green
+            radius=eff_link_size,
+            color=color.blue
         )
         self.frame2 = cylinder(
             pos=vector(0, 0, 0),
             axis=vector(0, 0, 0),
-            radius=link_size,
-            color=color.green
+            radius=eff_link_size,
+            color=color.blue
         )
         self.frame3 = cylinder(
             pos=vector(0, 0, 0),
             axis=vector(0, 0, 0),
-            radius=link_size,
-            color=color.green
+            radius=eff_link_size,
+            color=color.blue
         )
 
     def update_positions(self, pos):
--- main.py
+++ main.py
@@ -14,6 +14,21 @@ scene_canvas = canvas.get_selected()
 scene_canvas.width = 1080  
 scene_canvas.height = 720 
 
+
+scene_canvas.camera.pos = vector(200, -1100, -100) 
+scene_canvas.camera.axis = vector(0, 1800, -550) 
+
+# # Function to handle keyboard input for zooming
+# def zoom(event):
+#     if event.key == 'up':
+#         scene.fov *= 1.1  # Zoom in
+#     elif event.key == 'down':
+#         scene.fov /= 1.1  # Zoom out
+
+# # Bind the zoom function to the keyboard events
+# scene.bind('keydown', zoom)
+
+
 angle1 = 90
 angle2 = 45
 angle3 = 0
@@ -22,24 +37,25 @@ da2 = 1
 da3 = 1
 
 while True:
+
     rate(60)
-    if angle1 > 90:
+    if angle1 > 120:
         da1 = -da1
-    elif angle1 < 0:
+    elif angle1 < -45:
         da1 = -da1
 
     
-    if angle2 > 90:
+    if angle2 > 120:
         da2 = -da2
-    elif angle2 < 0:
+    elif angle2 < -45:
         da2 = -da2
     
-    if angle3 > 90:
+    if angle3 > 120:
         da3 = -da3
-    elif angle3 < 0:
+    elif angle3 < -45:
         da3 = -da3
 
-    # print(angle)
+
     rf1_pos,rf2_pos,rf3_pos,pos = delta_calculate.delta_calcForward(angle1, angle2,angle3)
     rf.update_positions(rf1_pos,rf2_pos,rf3_pos)
     re.update_positions(rf1_pos,rf2_pos,rf3_pos,pos)
