8b799d8f013fdd78bd6ac448ff1e98f0989eb144
ongsa12342 <61608951+ongsa12342@users.noreply.github.com>
2023-12-03 02:05:08 +0700

    move L(fake)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ base = BASE()
 rf = RF()
 re = RE()
 eff = EFF()
+
 scene_canvas = canvas.get_selected()
 scene_canvas.width = 1080  
 scene_canvas.height = 480 
@@ -23,12 +24,6 @@ theta2 = 0
 theta3 = 0
 
 
-angle1 = 90
-angle2 = 45
-angle3 = 0
-da1 = 1
-da2 = 1
-da3 = 1
 
 _,_,_,[x_in, y_in, z_in] = delta_calculate.delta_calcForward(0, 0, 0)
 
@@ -76,6 +71,37 @@ def button_get():
 
 calc_button = button(bind=button_get,text="Enter")
 
+
+wtext(text='     ' )
+wtext(text='Destination Input    :  ' )
+des_x = winput(text = 0 , bind= lambda: None, prompt='x')
+des_y = winput(text = 0 ,bind= lambda: None, prompt='y')
+des_z = winput(text = 0 ,bind= lambda: None, prompt='z')
+way = sphere(pos = vector(0,0,0), radius = 10,color = color.blue,emissive=True)
+way.emissive = 1
+traject = []
+def traj():
+    global traject
+
+    # Specify the start and end values for each dimension
+    x_start, x_end = pos[0], float(des_x.text)
+    y_start, y_end = pos[1], float(des_y.text)
+    z_start, z_end = pos[2], float(des_z.text)
+
+    # Specify the number of points
+    num_points = 120  # Adjust this as needed
+
+    # Generate linearly spaced values for each dimension
+    x_values = np.linspace(x_start, x_end, num_points)
+    y_values = np.linspace(y_start, y_end, num_points)
+    z_values = np.linspace(z_start, z_end, num_points)
+
+    # Combine the values into a single array
+    traject = np.column_stack((x_values, y_values, z_values))
+
+go_button = button(bind=traj,text="Enter")
+
+
 wtext(text='\n\n Angle Input    :  ' )
 in_theta0 = winput(bind= lambda: None, prompt='x',text=f'{0:.2f}')
 in_theta1 = winput(bind= lambda: None, prompt='y',text=f'{0:.2f}')
@@ -89,14 +115,22 @@ def theta_get():
 
     theta1, theta2, theta3 = float(in_theta0.text), float(in_theta1.text),float(in_theta2.text)
     _,_,_, p = delta_calculate.delta_calcForward(theta1, theta2, theta3)
-    in_pos_update(p)
 
 theta_button = button(bind=theta_get,text="Enter")
 
 while True:
     rate(60)
-    
+    if len(traject) > 0:
+        print(traject)
+        theta1,theta2,theta3 = delta_calculate.delta_calcInverse( traject[0][0],traject[0][1],  traject[0][2])
+        traject = traject[1:]
+        in_theta_update()
+        in_pos_update(pos)
+        
+
+    way.pos = vector(float(des_x.text),float(des_y.text),float(des_z.text))
     rf1_pos,rf2_pos,rf3_pos,pos = delta_calculate.delta_calcForward(theta1, theta2, theta3)
+    
     rf.update_positions(rf1_pos,rf2_pos,rf3_pos)
     re.update_positions(rf1_pos,rf2_pos,rf3_pos,pos)
     eff.update_positions(pos)
@@ -104,8 +138,3 @@ while True:
     wt.text = f'(x, y, z,) = ({pos[0]:.2f}, {pos[1]:.2f}, {pos[2]:.2f})\n' 
     
     
-    # print(x_in,y_in,z_in)
-
-    angle1 += da1
-    angle2 += da2
-    angle3 += da3
